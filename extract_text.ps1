33bd3b5280f4305a60e8081ea1c201271a25b20a
Quan95974@ <quankietsuat@gmail.com>
2026-07-19 04:33:46 +0700

    feat: update frontend components and pages

--- extract_text.ps1
+++
@@ -1,4 +0,0 @@
-$doc = [xml](Get-Content "d:\Objects\eventSourcing\docx_temp\word\document.xml")
-$texts = $doc.GetElementsByTagName("w:t") | Select-Object -ExpandProperty InnerText
-$texts | Out-String -Width 50000 | Set-Content "d:\Objects\eventSourcing\docx_text.txt"
-Write-Host "Extracted text to docx_text.txt"
